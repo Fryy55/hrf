c7dbe09761caea7188946ffdcc7ed9683648d286
Fryy_55 <3pm.fryy@gmail.com>
2026-07-11 02:06:00 +0500

    Add icons to Linux registration

--- registration/linux.sh
+++ registration/linux.sh
@@ -9,10 +9,12 @@ echo '<?xml version="1.0" encoding="utf-8"?>
 <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
   <mime-type type="application/x.heart-rate-format-1">
     <comment>Heart Rate Format (HRF) v1 File</comment>
-    <glob pattern="*.hrf"/>
+    <glob pattern="*.hrf" />
     <magic priority="80">
-      <match type="string" offset="0" value="\x01HRF\x06\x07TR"/>
+      <match type="string" offset="0" value="\x01HRF\x06\x07TR" />
     </magic>
+    <icon name="media-floppy" />
+    <generic-icon name="application-x-object" />
   </mime-type>
 </mime-info>' > $HOME/.local/share/mime/packages/x.heart-rate-format.xml
 update-mime-database $HOME/.local/share/mime
